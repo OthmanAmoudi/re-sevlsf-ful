8c936deb69aa69d0a87067a004913bbb268c8511
Othman <red.fantastic@hotmail.com>
2020-06-10 11:38:14 +0300

    base syles and pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Game from "./pages/Game";
+import GameOver from "./pages/GameOver";
+import Home from "./pages/Home";
+import HighScores from "./pages/HighScores";
+import Navbar from "./components/Navbar";
+import { Container } from "./styled/Container";
+import { Main } from "./styled/Main";
+import Global from "./styled/Global";
 
 function App() {
-  return <div className="App">HIII</div>;
+  return (
+    <BrowserRouter>
+      <Global />
+      <Main>
+        <Container>
+          <Navbar />
+          <Switch>
+            <Route path="/game" component={Game} />
+            <Route path="/highscores" component={HighScores} />
+            <Route path="/gameover" component={GameOver} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Container>
+      </Main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
